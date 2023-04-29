ac22f305c13e4c0662aecfd83d2ea530d10e69e9
Josy-Yuuka <yuukatitania@gmail.com>
2023-04-29 09:17:59 -0300

    codCompleto

--- src/App.java
+++ src/App.java
@@ -1,16 +1,72 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public class App {
-    public static void main(String[] args){
-       
-        Scanner ler = new Scanner(System.in);
+public class App{
+    public static void main(String[] args) {
+
+        Scanner lernome = new Scanner (System.in);
+        Scanner lerpalpite = new Scanner (System.in);
+        Random num = new Random();
+
+
+        int numero = num.nextInt(2);
+        //System.out.println(numero);
+
         Jogador jogador1 = new Jogador();
+        Jogador jogador2 = new Jogador();
 
 
-        System.out.println("Digite o nome: ");
-        jogador1.setNome(ler.nextLine()); 
-        jogador1.mostrarDados();
+        do{
+            if(jogador1.getNome() == ""){
+                System.out.println("Digite seu nome Jogador 1: ");
+                String nome1 = lernome.nextLine();           
+                jogador1.setNome(nome1);    
+                System.out.println("Olá "+jogador1.getNome()+", bem vindo!");
 
-        ler.close();
+            }else{
+                System.out.println("Olá "+jogador1.getNome()+", bem vindo!");
+            };
+
+            System.out.println("Cara ou Coroa?");
+            System.out.println("0 - Cara");
+            System.out.println("1 - Coroa");
+            int palpite = lerpalpite.nextInt();
+            jogador1.setPalpite(palpite);
+
+
+        }while (jogador1.getPalpite() > 1 || jogador1.getPalpite() < 0);
+
+
+        do{
+            if(jogador2.getNome() == ""){
+                System.out.println("Digite seu nome Jogador 2: ");
+                String nome2 = lernome.nextLine();           
+                jogador1.setNome(nome2);    
+                System.out.println("Olá "+jogador2.getNome()+", bem vindo!");
+
+            }else{
+                System.out.println("Olá "+jogador2.getNome()+", bem vindo!");
+            };
+
+            System.out.println("Cara ou Coroa?");
+            System.out.println("0 - Cara");
+            System.out.println("1 - Coroa");
+            int palpite = lerpalpite.nextInt();
+            jogador2.setPalpite(palpite);
+
+                if(jogador1.getPalpite() == jogador2.getPalpite()){
+                    System.out.println(jogador1.getNome() + "Já escolheu essa opcao, escolha a                         outra");
+                }
+
+        }while (jogador2.getPalpite() > 1 || jogador2.getPalpite() < 0 || jogador1.getPalpite() == jogador2.getPalpite());
+
+
+        
+
+
+        
+
+        lernome.close();
+        lerpalpite.close();
     }
 }
