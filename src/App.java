82f35880f8e01db8a7e041a6bbdca19bf984636f
Josy-Yuuka <yuukatitania@gmail.com>
2023-04-29 09:18:21 -0300

    tudo mudoou

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.lang.Thread;
 
 public class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner lernome = new Scanner (System.in);
         Scanner lerpalpite = new Scanner (System.in);
@@ -41,7 +42,7 @@ public class App{
             if(jogador2.getNome() == ""){
                 System.out.println("Digite seu nome Jogador 2: ");
                 String nome2 = lernome.nextLine();           
-                jogador1.setNome(nome2);    
+                jogador2.setNome(nome2);    
                 System.out.println("Olá "+jogador2.getNome()+", bem vindo!");
 
             }else{
@@ -55,12 +56,31 @@ public class App{
             jogador2.setPalpite(palpite);
 
                 if(jogador1.getPalpite() == jogador2.getPalpite()){
-                    System.out.println(jogador1.getNome() + "Já escolheu essa opcao, escolha a                         outra");
+                    System.out.println(jogador1.getNome() + " Já escolheu essa opcao, escolha a outra");
                 }
 
         }while (jogador2.getPalpite() > 1 || jogador2.getPalpite() < 0 || jogador1.getPalpite() == jogador2.getPalpite());
 
+        System.out.println("Jogando a moeda..");
+        Thread.sleep(2000);
 
+        if(numero == jogador1.palpite){
+            System.out.println(jogador1.getNome()+" ganhou!!" );
+                if(jogador1.palpite == 0){
+                    System.out.println("Deu Cara!");
+                }else{
+                    System.out.println("Deu Coroa!");
+                }
+        }
+        if(numero == jogador2.palpite){
+            System.out.println(jogador2.getNome()+" ganhou!!" );
+                if(jogador2.palpite == 0){
+                    System.out.println("Deu Cara!");
+                }else{
+                    System.out.println("Deu Coroa!");
+                }
+        }
+        
         
 
 
